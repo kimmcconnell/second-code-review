ebbd89c14a4e1d3e659d52d064054817fe1685f0
Kimberly McConnell <kmcconnell393@gmail.com>
2018-09-22 12:00:55 -0700

    add divs for different biomes with images that will be shown as 'answers' to the quiz

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@
 //UI logic here
 
 $(document).ready( function() {
-
+  $("nameform").submit( function(event) {
+    event.preventDefault();
+    $(".biomes").hide;
+    var name = $("nameform input").val();
+  })
 
 });
