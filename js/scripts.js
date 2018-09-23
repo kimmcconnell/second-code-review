83d704073760b4161dc9948291be5153bddb8cfe
Kimberly McConnell <kmcconnell393@gmail.com>
2018-09-22 17:51:32 -0700

    add parseInt to make variable score return a number value that is the addition of all answers

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,14 @@ $(document).ready( function() {
 
   $("#quizform").submit( function(event) {
     event.preventDefault();
+    var answer1 = parseInt($("input:radio[name=q1radios]:checked").val());
+    var answer2 = parseInt($("input:radio[name=q2radios]:checked").val());
+    var answer3 = parseInt($("input:radio[name=q3radios]:checked").val());
+    var answer4 = parseInt($("input:radio[name=q4radios]:checked").val());
+    var answer5 = parseInt($("input:radio[name=q5radios]:checked").val());
+    score = answer1 + answer2 + answer3 + answer4 + answer5;
 
-    var answer1 = $("input:radio[name=q1radios]:checked").val();
-    console.log(answer1);
+    console.log(score);
 
   });
 
