e542915f00e42706543d0589a67be73518c6e6b9
Kimberly McConnell <kmcconnell393@gmail.com>
2018-09-22 17:28:41 -0700

    add basic logic for submits and create var score for totaling answer values

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 //Buisness logic here
 
-
+var score = 0;
 
 
 //UI logic here
@@ -10,7 +10,17 @@ $(document).ready( function() {
   $("#nameform").submit( function(event) {
     event.preventDefault();
     $(".biomes").hide();
+
     var name = $("#nameform input").val();
-  })
+    $(".name").append(name);
+  });
+
+  $("#quizform").submit( function(event) {
+    event.preventDefault();
+
+    var answer1 = $("input:radio[name=q1radios]:checked").val();
+    console.log(answer1);
+
+  });
 
 });
